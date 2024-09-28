ff93ee267d03f8c2aa405e0de04c7b1793277553
prathamrao021 <prathamrao021@gmail.com>
2024-09-28 17:28:17 -0400

    Error fixed

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,11 +1,12 @@
 import { Button } from "@/components/ui/button"
 import Link from 'next/link'
-
+import React from 'react'
+import Register from '@/app/register/page'
 export default function Home() {
   return (
     <>
     <div>
-      <Button>Click me</Button>
+      <Button>Click ME</Button>
     </div>
     </>
   )
