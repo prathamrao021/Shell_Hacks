6d3392b62ce0d698550eabd1ab1160c65b33cb0a
prathamrao021 <prathamrao021@gmail.com>
2024-09-29 00:28:27 -0400

    Time Available

--- front/src/app/choose/page.tsx
+++ front/src/app/choose/page.tsx
@@ -1,17 +1,31 @@
+"use client"
 import { Button } from "@/components/ui/button"; 
+import Link from 'next/link';
+import React from "react";
 
 export default function ChoosePage() {
+  
+  const handleVolunteer = async()=> {
+    localStorage.setItem('role', 'volunteer');
+    window.location.href = "/available";
+  }
+
+  const handleRecruiter = async()=> {
+    localStorage.setItem('role', 'recruiter');
+    window.location.href = "/ListEvent";
+  }
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen p-4 bg-background text-foreground">
       <header className="text-center mb-10">
         <h1 className="text-4xl font-bold">How can we assist you?</h1>
         <p className="mt-2 text-lg">Select an option below that best describes your intention.</p>
       </header>
-      <main className="space-y-4">
-        <Button variant="default" size="lg">
-          Volunteer
-        </Button>
-        <Button variant="secondary" size="lg">
+      <main className="space-y-4"> 
+          <Button variant="default" size="lg" onClick={handleVolunteer}>
+            Volunteer
+          </Button>
+        <Button variant="secondary" size="lg" onClick={handleRecruiter}>
           Need Help
         </Button>
       </main>
